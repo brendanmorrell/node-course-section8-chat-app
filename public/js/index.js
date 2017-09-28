ef839cd7f28305e806def70208575b3100444e29
Brendan Morrell <brendanmorrell@gmail.com>
2017-09-28 14:34:47 -0600

    Scroll to bottom if user is close to bottom and show new message counter if new message sent while user not at bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,75 @@
 //this fucntion tells the page to initiate thre request from client to server to open up the web socket and keep it open
-var socket = io();
+const socket = io();
+
+
+
+let counter=0
+
+let messageVariables = {
+
+}
+
+$('#messages').scroll( function () {
+  let messages = $('#messages');
+  let newMessage = messages.children('li:last-child');
+
+  let clientHeight = messages.prop('clientHeight');
+  let scrollTop = messages.prop('scrollTop');
+  let scrollHeight = messages.prop('scrollHeight');
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight();
+  if (scrollTop+clientHeight+newMessageHeight+lastMessageHeight >= scrollHeight) {
+    counter = 0;
+  }
+});
+
+
+function scrollToBottom () {
+  let messages = $('#messages');
+  let newMessage = messages.children('li:last-child');
+
+  let clientHeight = messages.prop('clientHeight');
+  let scrollTop = messages.prop('scrollTop');
+  let scrollHeight = messages.prop('scrollHeight');
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight();
+
+
+  if (scrollTop+clientHeight+newMessageHeight+lastMessageHeight >= scrollHeight) {
+    counter = 0;
+    messages.scrollTop(scrollHeight);
+  } else if (scrollTop+clientHeight+newMessageHeight+lastMessageHeight < scrollHeight) {
+    counter++;
+    if (counter > 1) {
+      let template = $('#notifier-message-template').html();
+      let html = Mustache.render(template, {
+        notifier: "New Message",
+        counter
+      });
+      $('#notifierMessageLocation').append(html);
+      window.setTimeout(() => {
+        //$('#newMessageNotifier').remove();
+        $('#notifierMessageLocation').children().fadeOut(2000, function () {
+          $('#newMessageNotifier').last().remove();
+        });
+      }, 1000)
+    } else if (counter <= 1) {
+      let template = $('#notifier-message-template').html();
+      let html = Mustache.render(template, {
+        notifier: "New Message"
+      });
+      $('#notifierMessageLocation').append(html);
+      $('#counter').remove();
+      window.setTimeout(() => {
+        //$('#newMessageNotifier').remove();
+        $('#notifierMessageLocation').children().fadeOut(2000, function () {
+          $('#newMessageNotifier').last().remove();
+        });
+      }, 1000)
+    }
+
+  };
+};
 
 
 socket.on('connect', function () {
@@ -20,6 +90,7 @@ socket.on('newMessage', function (message) {
   });
 
   $('#messages').append(html);
+  scrollToBottom ();
 });
 
 $('#message-form').on('submit', function (e) {
@@ -64,4 +135,5 @@ socket.on('newLocationMessage', function (message) {
     createdAt: formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom ();
 });
